c7d106b81b4ed8521a843cafac03ea2913b52a5d
stoff1180 <stoffmustapha1180@gmail.com>
2024-05-27 12:09:03 +0000

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    {
-      const task = true;
-      const task2 = false;
+    const task = true;
+    const task2 = false;
+    if (task && task2) {
+      return [task, task2];
     }
   }
 
